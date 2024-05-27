b21bbe10b77deb5f504d4ba1d8b323655a24e001
Srinu <sbudireddy6@gmail.com>
2024-05-27 12:47:15 +0530

    Add files via upload

---
+++ binary search.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+main()
+{
+	int a[5]={1,2,3,4,5},i,key=2,flag=0,mid=2;
+	{
+	//	for(i=0;i<5;i++)
+		{
+	if(key==mid)
+	{
+		flag=1;
+		//break;
+	}
+else if (key<mid)
+	{
+		flag=1;
+		//break;
+	}
+else if (key>mid)
+	{
+		flag=1;
+	//	break;
+		
+	}
+    }
+}
+    if(flag==1)
+    {
+    	printf("\n %d is position at is %d",a[i],i+1);
+	}
+	else
+	{
+		printf("the element is not found");
+	}
+	}
+
